4c21af5c844ff01af38d1a863941b14f0382023a
raajeshlr <raajeshlr2@gmail.com>
2021-09-23 22:50:49 +0530

    two test cases added

--- test_assignment.py
+++ test_assignment.py
@@ -1,10 +1,10 @@
 import os
 
-def check_if_data_exists():
+def test_if_data_exists():
     print(os.path.isdir('data'))
     assert os.path.isdir('data') != True
     
-def check_if_model_exists():
+def test_if_model_exists():
     print(os.path.isfile('model.h5'))
     assert os.path.isfile('model.h5') != True  
     
