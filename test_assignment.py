41330a92d7a8fb69b6d6ce9ffdd6cce67c1a3b63
raajeshlr <raajeshlr2@gmail.com>
2021-09-23 22:46:08 +0530

    two test cases added

--- test_assignment.py
+++ test_assignment.py
@@ -1,3 +1,12 @@
-def test_method():
-    name = 'gflow'
-    assert name == 'gflow'
+import os
+
+def check_if_data_exists():
+    print(os.path.isdir('data'))
+    assert os.path.isdir('data') != True
+    
+def check_if_model_exists():
+    print(os.path.isfile('model.h5'))
+    assert os.path.isfile('model.h5') != True  
+    
+    
+    
