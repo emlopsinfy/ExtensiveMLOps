0e26000f19e40535201d2e648a198af193cd2488
raajeshlr <raajeshlr2@gmail.com>
2021-09-24 20:53:38 +0530

    added new test case

--- test.py
+++ test.py
@@ -10,6 +10,10 @@ def test_if_model_exists():
     print(os.path.isfile('model.h5'))
     assert os.path.isfile('model.h5') != True  
     
+def test_if_model_exists():
+    print(os.path.isfile('bottleneck_features_train.npy'))
+    assert os.path.isfile('bottleneck_features_train.npy') != True      
+    
 def test_accuracy_score():
     df = pd.read_csv('metrics.csv')
     print((df['accuracy']>0.70))
