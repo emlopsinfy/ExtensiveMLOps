973202593c5fd1b7ae72ee0fd30a991eaa36033f
raajeshlr <raajeshlr2@gmail.com>
2021-09-24 21:02:00 +0530

    added test cases

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ def test_if_model_exists():
     print(os.path.isfile('model.h5'))
     assert os.path.isfile('model.h5') != True  
     
-def test_if_model_exists():
+def test_if_numpy_exists():
     print(os.path.isfile('bottleneck_features_train.npy'))
     assert os.path.isfile('bottleneck_features_train.npy') != True      
     
@@ -18,5 +18,15 @@ def test_accuracy_score():
     df = pd.read_csv('metrics.csv')
     print((df['accuracy']>0.70))
     assert (df['accuracy']>0.70).unique() == True
+    
+def test_train_model_accuracy():
+    df = pd.read_csv('metrics.csv')
+    max_acc = df[df.state == "train"]["accuracy"].max()
+    assert max_acc >= 0.70    
+    
+def test_valid_model_accuracy():
+    df = pd.read_csv('metrics.csv')
+    max_acc = df[df.state == "valid"]["accuracy"].max()
+    assert max_acc >= 0.70        
     
     
